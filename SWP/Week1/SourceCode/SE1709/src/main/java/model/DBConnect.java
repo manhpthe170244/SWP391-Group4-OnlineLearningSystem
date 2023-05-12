d19cc182ad1e7e0590bc716309769c9796b2b662
unknown <manhpthe170244@fpt.edu.vn>
2023-05-12 22:05:18 +0700

    chiu

--- SWP/Week1/SourceCode/SE1709/src/main/java/model/DBConnect.java
+++ SWP/Week1/SourceCode/SE1709/src/main/java/model/DBConnect.java
@@ -1,53 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author Phan Nguyen Tu Anh
- */
-class DBConnect {
-     public Connection conn=null;
-     public DBConnect(String url,String userName,String pass){
-         try {
-            String user = "sa";
-            String password= "123456";
-            String urlconnect = "jdbc:sqlserver://localhost:1433;databaseName=se1709";
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            conn = DriverManager.getConnection(url, user, pass);
-        } catch (Exception ex) {
-            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
-     public DBConnect(){
-        this("jdbc:sqlserver://localhost:1433;databaseName=se1709",
-                "sa","123456");
-    }
-    public ResultSet getData(String sql){
-        ResultSet rs=null;
+
+public class DBConnect {
+    protected Connection connection;
+    public DBConnect()
+    {
         try {
-            Statement state = conn.createStatement(
-                    ResultSet.TYPE_SCROLL_SENSITIVE,
-                    ResultSet.CONCUR_UPDATABLE);
-            rs=state.executeQuery(sql);
-        } catch (SQLException ex) {
-            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+            // Edit URL , username, password to authenticate with your MS SQL Server
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=CartDB";
+            String username = "sa";
+            String password = "sa";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
         }
-        return rs;
-    } 
-     
-    public static void main(String[] args) {
-        DBConnect dbConnect = new DBConnect();
     }
 }
