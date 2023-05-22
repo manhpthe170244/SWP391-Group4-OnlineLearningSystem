ea6d65d9d9cc03cb844f4e8db17600fb13134edd
mrchuu <soncqhe170808@fpt.edu.vn>
2023-05-22 14:16:25 +0700

    The servlet, dao, jsp Login

---
+++ SWP/Week2/SourceCode/SWP/src/java/controller/LoginController.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.UserDAO;
+import entity.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author admin
+ */
+public class LoginController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final UserDAO userDAO = new UserDAO();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+
+                request.getRequestDispatcher("login.jsp").forward(request, response);
+                
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+
+                String email = request.getParameter("email");
+                String password = request.getParameter("password");
+                UserDAO ud = new UserDAO();
+                User userLogin = ud.login(email, password);               
+                if(userLogin != null) {
+                    System.out.println("Login Success!");
+                    response.sendRedirect("login");
+                } else {
+                    request.setAttribute("err", "Wrong email or password!");
+                    request.getRequestDispatcher("login.jsp").forward(request, response);
+                }
+                
+                
+	}
+
+}
