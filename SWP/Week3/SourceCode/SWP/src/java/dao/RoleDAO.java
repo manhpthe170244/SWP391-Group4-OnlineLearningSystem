5e5ce05acae2faeb9ba82e380e88e3bc0885086a
mrchuu <soncqhe170808@fpt.edu.vn>
2023-05-24 23:07:43 +0700

    sua sourcecode week3

--- SWP/Week3/SourceCode/SWP/src/java/dao/RoleDAO.java
+++ SWP/Week3/SourceCode/SWP/src/java/dao/RoleDAO.java
@@ -12,8 +12,9 @@ import java.util.Vector;
  * @author FPT
  */
 public class RoleDAO extends MyDAO{
-    public Vector<Role> getAllRoleNotAdmin(){
-        xSql = "select * from role where role_name not like 'Guest' and role_name not like 'Admin'";
+    public Vector<Role> getRegisterRoles(){
+        xSql = "select * from role where role_name like 'Student' "
+                + "or role_name like 'Lecturer'";
         Vector<Role> rv = new Vector<>();
         int xroleId;
         String xroleName;
@@ -29,5 +30,24 @@ public class RoleDAO extends MyDAO{
             e.printStackTrace();
         }
         return rv;
+    }
+    public Role getRoleById(int roleId){
+        xSql = "select * from role where role_id = ?";
+        Role r = null;
+        int xroleId;
+        String xroleName;
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setInt(1, roleId);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                xroleId = rs.getInt("role_id");
+                xroleName = rs.getString("role_name");
+                r = new Role(roleId, xroleName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return r;
     }
 }
