2b57afe4dbf8f79d233d5c0f7680b57ef519a52d
Ngoc Linh <linhntnhe171088@fpt.edu.vn>
2023-06-01 16:42:37 +0700

    Update insertpricepackage.sql

--- SWP/Week4/SourceCode/insertpricepackage.sql
+++ SWP/Week4/SourceCode/insertpricepackage.sql
@@ -1,6 +1,7 @@
 SET IDENTITY_INSERT Price_Package ON
-INSERT INTO Price_Package 
-VALUES (1,N'Free',N'1 month',N'Active',N'Yes',N'Access to selected free courses for 1 month.'),
-	(2,N'Silver',N'3 months',N'Active',N'Yes',N'Access to silver level courses for 3 months with multiple device support'),
-	(3,N'Gold',N'6 months',N'Active',N'Yes',N'Access to gold level courses for 6 months with multiple device support and personalized learning plans.'),
-	(4,N'Diamond',N'Lifetime',N'Active',N'Yes',N'Unlimited lifetime access to all courses with multiple device support, exclusive bonuses, and personalized mentorship.');
+INSERT INTO Price_Package (package_id, package_name, duration, pack_status, multiple, description)
+VALUES (1,N'Free',1,1,0,N'Access to selected free courses for 1 month.'),
+	(2,N'Silver',3,1,0.8,N'Access to silver level courses for 3 months with multiple device support'),
+	(3,N'Gold',6,1,1,N'Access to gold level courses for 6 months with multiple device support and personalized learning plans.'),
+	(4,N'Diamond',1200,1,1.5,N'Unlimited lifetime access to all courses with multiple device support, exclusive bonuses, and personalized mentorship.');
+	select * from Price_Package
