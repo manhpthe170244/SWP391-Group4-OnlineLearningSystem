fd889e0d61c5e7a00fd9ba548c57c028c9415dde
unknown <manhpthe170244@fpt.edu.vn>
2023-06-01 17:16:22 +0700

    mm

--- SWP/Week4/SourceCode/insertpricepackage.sql
+++ SWP/Week4/SourceCode/insertpricepackage.sql
@@ -4,4 +4,3 @@ VALUES (1,N'Free',1,1,0,N'Access to selected free courses for 1 month.'),
 	(2,N'Silver',3,1,0.8,N'Access to silver level courses for 3 months with multiple device support'),
 	(3,N'Gold',6,1,1,N'Access to gold level courses for 6 months with multiple device support and personalized learning plans.'),
 	(4,N'Diamond',1200,1,1.5,N'Unlimited lifetime access to all courses with multiple device support, exclusive bonuses, and personalized mentorship.');
-	select * from Price_Package
