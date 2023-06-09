d1e2801724462790822618bbc8ee6eefa07acdbe
Ngoc Linh <linhntnhe171088@fpt.edu.vn>
2023-06-09 20:31:46 +0700

    linh

---
+++ SWP/Week5/SourceCode/SWP/src/java/dao/QuizResultDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import javax.xml.transform.Source;
+
+/**
+ *
+ * @author Uslaptop
+ */
+public class QuizResultDAO {
+    
+}
