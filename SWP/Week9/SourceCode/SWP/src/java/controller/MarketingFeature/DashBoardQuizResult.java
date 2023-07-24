7fd55d503670d39606bb8a1df608d6998880807a
soncqhe170808 <soncqhe170808@fpt.edu.vn>
2023-07-25 03:55:04 +0700

    aaa

--- SWP/Week9/SourceCode/SWP/src/java/controller/MarketingFeature/DashBoardQuizResult.java
+++ SWP/Week9/SourceCode/SWP/src/java/controller/MarketingFeature/DashBoardQuizResult.java
@@ -10,6 +10,7 @@ import dao.UserDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,6 +22,7 @@ import java.util.Map;
  *
  * @author FPT
  */
+@WebServlet(name = "savePostCategory", urlPatterns = {"/dashboardQuizResult"})
 public class DashBoardQuizResult extends HttpServlet {
     
     @Override
